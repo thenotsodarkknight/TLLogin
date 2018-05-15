287f728e97941f51808a5d2729b1655fda1762f9
Abeen Bhattacharya <abeen9@gmail.com>
2018-05-15 18:14:10 +0530

    c

--- my_project/settings.py
+++ my_project/settings.py
@@ -118,10 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT  = 'static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media/'
+STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
--- my_project/urls.py
+++ my_project/urls.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from django.conf.urls.static import static
-from django.conf.urls import url,include
-from . import settings
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
